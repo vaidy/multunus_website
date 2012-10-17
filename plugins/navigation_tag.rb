a5db4389933cf7faeed32dc4f46374fdb2232d48
Abhilash <abhilash.m@multunus.com>
2012-10-17 13:53:31 +0000

    Made changes to the open source page. Removed youngturks tab.

--- plugins/navigation_tag.rb
+++ plugins/navigation_tag.rb
@@ -13,8 +13,12 @@ module Jekyll
         active_status = (context['page']['nav'] == title) ? "active" : ""
         dropdown_status = nav_value[:sub_nav] ? "dropdown" : ""
         
-        navigation_markup += "<li class='#{active_status} #{dropdown_status}'><a href='#{address}' target='#{nav_value[:target]}'>#{title}</a>"
-
+        navigation_markup += "<li class='#{active_status} #{dropdown_status}'><a href='#{address}' target='#{nav_value[:target]}'>#{title}"
+        if nav_value[:show_new_icon]
+          navigation_markup += "<img src='/images/icon_new.png' class='new-icon'/></a>"
+        else
+          navigation_markup += "</a>"
+        end
         if nav_value[:sub_nav]
           navigation_markup += "<ul class='dropdown-menu'>"
           nav_value[:sub_nav].each do |subnav_key,subnav_value|
